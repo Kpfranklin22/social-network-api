d376899e7e491866593c231d0fe7d369f016d8dd
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-03 17:36:28 -0500

    factoring routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,20 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingletThought,
+  createThought,
+  updateThought,
+  deleteThought,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.use("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("./thoughtId")
+  .get(getSingletThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
