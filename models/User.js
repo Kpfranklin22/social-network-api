3e1d3951f3d1e92b9238ac123856078c168f467b
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-03 16:46:15 -0500

    factoring models - routing models/index

--- models/User.js
+++ models/User.js
@@ -1,27 +1,26 @@
 const { Schema, model } = require("mongoose");
+const thoughtSchema = require("./Thought");
 
-const userSchema = new Schema(
-    {
-        username: {
-            type:String,
-            unique: true,
-            required: true,
-            trimmed: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            validate: [isEmail, 'Invalid Email']
-        },
-        thoughts: {
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trimmed: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: [isEmail, "Invalid Email"],
+  },
+  thoughts: [thoughtSchema],
+  friends: [
 
-        },
-        friends: {
+    
+  ],
+});
 
-        }
-    });
+const User = model("user", userSchema);
 
-const User = model('user', userSchema);
-
-module.exports = User
+module.exports = User;
