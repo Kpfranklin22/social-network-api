24e1ce6c1db2a47e04abccee46d4ed0e50f7f245
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-03 17:23:48 -0500

    factoring userRoutes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,23 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+  addThought,
+  removeThought,
+} = require("../../controllers/userController");
 
-} = require('../../controllers/userController');
+// /api/users
+router.use("/").get(getUsers).post(createUser);
 
+// /api/users/:usersId
+router.route("/:userId").get(getSingleUser).delete(deleteUser);
+
+// /api/users/:userId/thoughts
+router.use("/:userId/thoughts").post(addThought);
+
+// /api/users/:userId/thoughts/:thoughtId
+router.use("/:userId/thoughts/:thoughtsId").delete(removeThought);
+
+module.exports = router
